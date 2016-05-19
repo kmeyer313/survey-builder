710eca25b705920abadcba3a6d50609a4dc21a55
Katie Meyer <katiemeyer@icloud.com>
2016-05-19 15:40:04 -0500

    Update survey migration

--- survey-builder/db/migrate/20160519142501_create_surveys.rb
+++ survey-builder/db/migrate/20160519142501_create_surveys.rb
@@ -1,16 +1,15 @@
 class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
-      t.integer     :course_id
-      t.string      :title
-      t.text        :description
+      t.string      :survey_title, null: false
+      t.text        :survey_description
       t.integer     :question_count
-      t.integer     :allowed_attempts
+      t.integer     :submissions_allowed
       t.boolean     :anonymous_submission
       t.boolean     :faculty_access
-      t.timestamps  :published_at
-
-      t.timestamps null: false
+      t.datetime    :created_at, null: false
+      t.datetime    :updated_at, null: false
+      t.datetime    :published_at
     end
   end
 end
